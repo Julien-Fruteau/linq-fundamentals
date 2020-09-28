6508258269f2dfc6961a93e2170ccbd3ce182d89
Julien FRUTEAU <20012454>
2020-09-28 14:04:50 +0200

    2nd commit

---
+++ src/Introduction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\20012454\Downloads";
+            ShowLargeFilesWithoutLinq(path);
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"{file.Name}, {file.Length}");
+            }
+        }
+    }
+}
