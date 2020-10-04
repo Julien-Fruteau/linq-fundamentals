cd2a8da81baba3d1a873130a3c4fc279fc82e738
julien <julien.fruteau@gmail.com>
2020-10-04 20:37:17 +0200

    an introduction

--- src/Introduction/Program.cs
+++ src/Introduction/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Linq;
 
 namespace Introduction
 {
@@ -17,17 +20,41 @@ namespace Introduction
                 rootPath = @"C:\Users\20012454\Downloads";
             }
             ShowLargeFilesWithoutLinq(rootPath);
+            Console.WriteLine("**********");
+            ShowLargeFilesWithLinq(rootPath);
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            // var query = from file in new DirectoryInfo(path).GetFiles()
+            //             orderby file.Length descending
+            //             select file;
+            var query = new DirectoryInfo(path).GetFiles()
+                            .OrderByDescending(x => x.Length)
+                            .Take(5);
+            foreach (var file in query.Take(5))
+            {
+                Console.WriteLine($"{file.Name.PadRight(50)} : {file.Length,15:N0}");
+            }
         }
 
         private static void ShowLargeFilesWithoutLinq(string path)
         {
             DirectoryInfo directory = new DirectoryInfo(path);
             FileInfo[] files = directory.GetFiles();
-
-            foreach (var file in files)
+            Array.Sort(files, new FileInfoComparer());
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine($"{file.Name}, {file.Length}");
+                Console.WriteLine($"{files[i].Name.PadRight(50)} : {files[i].Length, 15:N0}");
             }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare([AllowNull] FileInfo x, [AllowNull] FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
         }
     }
 }
