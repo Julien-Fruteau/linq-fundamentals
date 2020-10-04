5e01acf26a25795e5b798bd076b1a37808165ae6
julien <julien.fruteau@gmail.com>
2020-10-04 21:19:04 +0200

    IEnumerable : interface to hide the source of data

---
+++ src/Features/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee {Id = 1, Name = "Julien"},
+                new Employee {Id = 2, Name = "Bibi"}
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee {Id = 3, Name = "Lulu"}
+            };
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+        }
+    }
+}
