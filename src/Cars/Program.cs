5e85044d379c826e889bba6dcda7ee4151f333e3
julien <julien.fruteau@gmail.com>
2020-10-07 13:09:11 +0200

    ordering sequence, first and last

--- src/Cars/Program.cs
+++ src/Cars/Program.cs
@@ -10,9 +10,23 @@ namespace Cars
         static void Main(string[] args)
         {
             var cars = ProcessFile("src/Cars/fuel.csv");
-            foreach (var car in cars)
+
+            var query = from car in cars
+                        where car.Manufacturer == "BMW" && car.Year == 2016
+                        orderby car.Combined descending, car.Name
+                        select car;
+            var last = query.LastOrDefault();
+
+            var top = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                            .OrderByDescending(c => c.Combined)
+                            .ThenBy(c => c.Name)
+                            .First();
+
+            System.Console.WriteLine($"top : {top.Name} Vs last : {last.Name}");
+
+            foreach (var car in query.Take(10))
             {
-                System.Console.WriteLine(car.Name);
+                System.Console.WriteLine($"{car.Manufacturer.PadRight(10)} {car.Name.PadRight(30)} : {car.Combined}");
             }
         }
 
